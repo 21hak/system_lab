78c1e94b71f5265fd4388921b715d242f02a09db
21hak <2wonakk@gmail.com>
2020-10-14 18:56:22 +0900

    lab practice4

---
+++ hello_bof/cat.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int bof(void){
+    char buffer[16];
+
+    scanf("%s", buffer);
+    return 0;
+}
+
+int main(void){
+    bof();
+    return 0;
+}
---
+++ hello_bof/cat.s
@@ -0,0 +1,56 @@
+	.file	"cat.c"
+	.section	.rodata
+.LC0:
+	.string	"%s"
+	.text
+	.globl	bof
+	.type	bof, @function
+bof:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	leaq	-32(%rbp), %rax
+	movq	%rax, %rsi
+	movl	$.LC0, %edi
+	movl	$0, %eax
+	call	__isoc99_scanf
+	movl	$0, %eax
+	movq	-8(%rbp), %rdx
+	xorq	%fs:40, %rdx
+	je	.L3
+	call	__stack_chk_fail
+.L3:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	bof, .-bof
+	.globl	main
+	.type	main, @function
+main:
+.LFB1:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	call	bof
+	movl	$0, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 4.8.5 20150623 (Red Hat 4.8.5-36)"
+	.section	.note.GNU-stack,"",@progbits
