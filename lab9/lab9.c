673d935973f28e975559ed555814595a9c57b21d
21hak <2wonakk@gmail.com>
2020-11-18 21:13:20 +0900

    lab9-2

--- lab9/lab9.c
+++ lab9/lab9.c
@@ -2,17 +2,24 @@
 #include <unistd.h>
 #include <sys/types.h>
 
-int main(){
-    pid_t pid;
-    int x;
-    x = 0;
-    x++;
-    pid = fork();
-    if(pid == 0){
-        x++;
-        printf("child PID : %ld, x : %d\n", getpid(), x);
-        return 0;
-    }
-    printf("parent PID : %ld, x : %d\n", getpid(), x);
-    return 0;
+// int main(){
+//     pid_t pid;
+//     int x;
+//     x = 0;
+//     x++;
+//     pid = fork();
+//     if(pid == 0){
+//         x++;
+//         printf("child PID : %ld, x : %d\n", getpid(), x);
+//         return 0;
+//     }
+//     printf("parent PID : %ld, x : %d\n", getpid(), x);
+//     return 0;
+// }
+
+int main(int argc, char *argv[]){
+    char* execve_argv[] = {"-a"};
+    char* execve_environ[] = {"0"};
+
+    execev("/bin/ls", execve_argv, execve_environ);
 }
