6bf2751ffe7a38ff25c184186d059b57045eae7a
21hak <2wonakk@gmail.com>
2020-11-18 21:35:39 +0900

    lab9-3

--- lab9/lab9.c
+++ lab9/lab9.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <signal.h>
+#include <stdlib.h>
 
 // int main(){
 //     pid_t pid;
@@ -17,10 +19,17 @@
 //     return 0;
 // }
 
-int main(int argc, char *argv[]){
-    char* execve_argv[] = {"ls", "-al"};
-    char* execve_environ[] = {"0"};
+// int main(int argc, char *argv[]){
+//     char* execve_argv[] = {"ls", "-al"};
+//     char* execve_environ[] = {"0"};
 
-    execve("/bin/ls", execve_argv, execve_environ);
+//     execve("/bin/ls", execve_argv, execve_environ);
+//     return 0;
+// }
+int main(int argc, char *argv[]){
+    if(argc>2){
+        printf("%d %d", atoi(argv[1]), atoi(argv[2]));
+        kill(atoi(argv[1]), atoi(argv[2]));
+    }
     return 0;
 }
