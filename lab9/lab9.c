2614a25dd9944994601825953c9d157d3acfc42a
21hak <2wonakk@gmail.com>
2020-11-25 18:09:28 +0900

    lab10

--- lab9/lab9.c
+++ lab9/lab9.c
@@ -4,31 +4,30 @@
 #include <signal.h>
 #include <stdlib.h>
 
-// int main(){
-//     pid_t pid;
-//     int x;
-//     x = 0;
-//     x++;
-//     pid = fork();
-//     if(pid == 0){
-//         x++;
-//         printf("child PID : %ld, x : %d\n", getpid(), x);
-//         return 0;
-//     }
-//     printf("parent PID : %ld, x : %d\n", getpid(), x);
-//     return 0;
-// }
+int main(){
+    pid_t pid;
+    int x;
+    x = 0;
+    x++;
+    pid = fork();
+    if(pid == 0){
+        x++;
+        printf("child PID : %ld, x : %d\n", getpid(), x);
+        return 0;
+    }
+    printf("parent PID : %ld, x : %d\n", getpid(), x);
+    return 0;
+}
 
-// int main(int argc, char *argv[]){
-//     char* execve_argv[] = {"ls", "-al"};
-//     char* execve_environ[] = {"0"};
+int main(int argc, char *argv[]){
+    char* execve_argv[] = {"ls", "-al"};
+    char* execve_environ[] = {"0"};
 
-//     execve("/bin/ls", execve_argv, execve_environ);
-//     return 0;
-// }
+    execve("/bin/ls", execve_argv, execve_environ);
+    return 0;
+}
 int main(int argc, char *argv[]){
     if(argc>2){
-        printf("%d %d", atoi(argv[1]), atoi(argv[2]));
         kill(atoi(argv[1]), atoi(argv[2]));
     }
     return 0;
